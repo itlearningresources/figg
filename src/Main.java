96cb5230ad558229a496e04732be50e3522361c3
itlearningresources <mail@itlearningresources.com>
2018-12-30 10:32:28 -0500

    Update

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for ( int i=0;i<args.length;i++ ) {
+            System.out.println( "argument " + i + ": " + args[i] );
+        }
+
+    }
+
+
+
+}
